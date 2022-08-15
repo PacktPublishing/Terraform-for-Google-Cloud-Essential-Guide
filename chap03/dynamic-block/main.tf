2b1fbde34f943caef89773ea18a67df23503f2e0
Radek Simko <radek.simko@gmail.com>
2022-08-15 11:00:56 +0200

    make code slightly more elegant w/ direct references

--- chap03/dynamic-block/main.tf
+++ chap03/dynamic-block/main.tf
@@ -1,6 +1,4 @@
 resource "google_compute_instance" "this" {
-  depends_on = [google_compute_disk.this]
-
   name         = var.server_name
   machine_type = var.machine_type
   zone         = var.zone
@@ -12,25 +10,25 @@ resource "google_compute_instance" "this" {
   }
 
   # attached_disk {
-  #   source = "small-disk"
-  #   mode   = var.disks["small-disk"]["mode"]
+  #   source = google_compute_disk.this["small-disk"].name
+  #   mode   = var.disks["small-disk"].mode
   # }
 
   # attached_disk {
-  #   source = "medium-disk"
-  #   mode   = var.disks["medium-disk"]["mode"]
+  #   source = google_compute_disk.this["medium-disk"].mode
+  #   mode   = var.disks["medium-disk"].mode
   # }
 
   # attached_disk {
-  #   source = "large-disk"
-  #   mode   = var.disks["large-disk"]["mode"]
+  #   source = google_compute_disk.this["large-disk"].mode
+  #   mode   = var.disks["large-disk"].mode
   # }
 
   dynamic "attached_disk" {
-    for_each = var.disks
+    for_each = google_compute_disk.this
     content {
       source = attached_disk.key
-      mode   = attached_disk.value["mode"]
+      mode   = var.disks[attached_disk.key].mode
     }
   }
 
