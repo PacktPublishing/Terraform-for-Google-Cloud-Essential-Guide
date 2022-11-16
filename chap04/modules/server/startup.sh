db25bc783ba33abeaf73bd52a9af332a340f4bbc
Bernd Nordhausen (Gmail) <bernd.nordhauen@gmail.com>
2022-11-16 11:01:21 +0800

    version 1.0.0 added

--- chap04/modules/server/startup.sh
+++ chap04/modules/server/startup.sh
@@ -2,4 +2,4 @@
 apt update
 apt -y install apache2
 cat <<EOF > /var/www/html/index.html
-<html><body><p>Hello World!</p><p>This is version 2.0.0</p></body></html>
+<html><body><p>Hello World!</p><p>This is version 1.0.0</p></body></html>
