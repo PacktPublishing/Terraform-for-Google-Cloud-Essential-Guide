22d92912f7068f2c41f0fdaf3f33292d7e027179
Bernd Nordhausen (Gmail) <bernd.nordhauen@gmail.com>
2022-11-19 16:37:12 +0800

    .

--- chap06/main/provider.tf
+++ chap06/main/provider.tf
@@ -1,1 +1,21 @@
-../provider.tf
+terraform {
+  required_version = "~> 1.3.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.40"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+provider "google-beta" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
